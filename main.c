6823aa276fe66e3cf33b7b2fb4b4fb4212bd922e
Abhishek6922 <76235557+Abhishek6922@users.noreply.github.com>
2022-01-16 13:09:39 +0530

    few modifications

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<ctype.h>
 
 typedef struct node
 {
@@ -11,7 +12,7 @@ typedef struct node
 node *first1,*first2,*first3;
 node *temp1,*temp2,*temp3;
 
-int disk;
+int disk,counter;
 
 node *ins_f(node *first,int data)
 {
@@ -30,8 +31,8 @@ FILE * printt(int data,FILE *f)
     //q=fopen("output.txt","a");
     if(data==0)
     {
-        printf("...................\t\t");
-        fprintf(f,"...................\t\t");
+        printf("        |+|          \t\t");
+        fprintf(f,"        |*|          \t\t");
     }
 
     else
@@ -69,16 +70,16 @@ void dis()
     f=fopen("output","a+");
     printf("\n");
   // printf(f,"\n");
-    printf("      SOURCE");
-    fprintf(f,"\n      SOURCE");
-    printf("------------------------");
-    fprintf(f,"------------------------");
-    printf("AUXILARY");
-    fprintf(f,"AUXILARY");
-    printf("-----------------------");
-    fprintf(f,"----------------------");
-    printf("DESTINATION");
-    fprintf(f,"DESTINATION");
+    printf("[::::::SOURCE::::::]");
+    fprintf(f,"\n[::::::SOURCE::::::]");
+    printf("___________");
+    fprintf(f,"___________");
+    printf("[::::::AUXILARY::::::]");
+    fprintf(f,"[::::::AUXILARY::::::]");
+    printf("_______");
+    fprintf(f,"_______");
+    printf("[::::::DESTINATION::::::]");
+    fprintf(f,"[::::::DESTINATION::::::]");
         printf("\n\n\n");
     fprintf(f,"\n\n\n");
     fclose(f);
@@ -86,17 +87,20 @@ void dis()
 
 void picker(node *first,int peg)
 {
+    //printf("picker starts\n");
      node *temp;
     temp =(node *)malloc(sizeof(node));
     temp=first;
     while(temp->data!=peg)
         temp=temp->next;
             temp->data=0;
+      //      printf("picker(endeds\n)");
 //return tempo;
 }
 
 void placer(node *first,int peg)
 {
+    //printf("placers starts\n");
   node *temp,*perv;
   perv=(node *)malloc(sizeof(node));
   perv=NULL;
@@ -107,10 +111,16 @@ void placer(node *first,int peg)
        perv=temp;
         temp=temp->next;
    }
+   if(disk==1)
+   {
+       temp->data=peg;
+       return;
+   }
    if(temp->data>perv->data)
       perv->data=peg;
    else
     temp->data=peg;
+    //printf("placer ends\n");
 }
 
 void mover(char src, char des, int peg)
@@ -147,47 +157,63 @@ void toh(int n,char sr,char au, char de,int data)
 
     if(n==1)
        {
+           ++counter;
+         printf("<------STEP NUMBER :%d----->\n\n",counter);
         printf("\n%d FROM SOURCE TO DESTINATION %c to %c",n,sr,de);
         mover(sr,de,n);
         return;
        }
 toh(n-1,sr,de,au,data);
+           ++counter;         printf("<------STEP NUMBER :%d------>\n\n",counter);
     printf("\n%d FROM SOURCE TO DESTINATION %c to %c:",n,sr,de);
    mover(sr,de,n);
     toh(n-1,au,sr,de,data);
 }
 int main()
 {
+    while(1)
+    {
     FILE *f;
     f=fopen("output","a");
     int data;
-    int i;
-
+    int i,count=0;
+    counter=count;
     first1=(node *)malloc(sizeof(node));
     first2=(node *)malloc(sizeof(node));
     first3=(node *)malloc(sizeof(node));
     first1=NULL;
     first2=NULL;
     first3=NULL;
-    printf("NUMBER OF DISKS PLEASE:");
+
+    printf("NUMBER OF DISKS PLEASE(Number greater than zero):");
     scanf("%d",&disk);
+    printf("---------------------------------------------------");
     fprintf(f,"---------------------------------------------------");
+    printf("TOWER OF HANOI FOR %d DISK",disk);
     fprintf(f,"TOWER OF HANOI FOR %d DISK",disk);
+    printf("---------------------------------------------------\n\n\n");
     fprintf(f,"---------------------------------------------------\n\n\n");
     fclose(f);
 
     for(i=disk;i>0;i--)
     {
         data=i;
-        first1 =ins_f(first1,data);
+        first1=ins_f(first1,data);
         first2=ins_f(first2,0);
         first3=ins_f(first3,0);
     }
     dis();
     toh(disk,'A','B','C',data);
-    printf("ended\n");
+  //  printf("ended\n");
+    temp1=first1;
+    temp2=first2;
+    temp3=first3;
+    free(temp1);
+    free(temp2);
+    free(temp3);
 
-
+fclose(f);
     printf("Hello world!\n");
+    }
     return 0;
 }
